761a7c0e0b2d6cc8b9e0e496120e9b29086d9a44
Marc Catrisse <marc.catrisse@upc.edu>
2024-07-03 00:55:28 +0200

    fix install unzip

--- src/init_script.sh
+++ src/init_script.sh
@@ -5,7 +5,7 @@ sudo apt-get -y upgrade
 
 # Install awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+sudo apt install -y unzip && unzip awscliv2.zip
 sudo ./aws/install
 
 
