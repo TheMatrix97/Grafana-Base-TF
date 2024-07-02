8bc2e043775083eda15c99cffa6141193500c406
Marc Catrisse <marc.catrisse@upc.edu>
2024-07-03 01:25:48 +0200

    add batch flags

--- src/init_script.sh
+++ src/init_script.sh
@@ -10,7 +10,7 @@ sudo apt-get -y remove docker docker-engine docker.io containerd runc
 # Install Docker
 sudo apt-get -y install ca-certificates curl gnupg lsb-release
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
